4b774515bed4ad23a00669bc3578be1e4133814e
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-04 19:51:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int arr[1001];
+	arr[1] = 1;
+	arr[2] = 2;
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i++)
+	{
+		arr[i] = (arr[i - 1] + arr[i - 2])%10007;
+	}
+	cout << arr[n];
+}
