545acaaf8a93c18a56ab319a406aeedb68aab09f
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-09 14:15:40 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,4 +1,4 @@
 s=input()
-alpha=[chr(ord('a')+i) for i in range(26)]
+alpha=[chr(ord('a')+i) for i in range(26)] #모든 알파벳을 저장한다. 
 for i in alpha:
-    print(s.find(i), end=" ")
+    print(s.find(i), end=" ") #해당 알파벳이 존재하지 않으면 -1, 존재하면 인덱스 출력력
