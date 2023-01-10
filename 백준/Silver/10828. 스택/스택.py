ecc94585b7b76789103a9959a9a82d94582df5c7
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-11 01:11:59 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,23 @@
+import sys
+n=int(input())
+stack=[]
+for _ in range(n):
+    temp=sys.stdin.readline().split()
+    if len(temp)==2:
+        stack.append(temp[1])
+    elif temp[0]=="pop":
+        if len(stack)==0:
+            print(-1)
+            continue
+        print(stack.pop())
+    elif temp[0]=="size":
+        print(len(stack))
+    elif temp[0]=="empty":
+        print("1" if len(stack)==0 else "0")
+    else:
+        if len(stack)==0:
+            print(-1)
+            continue
+        res=stack.pop()
+        print(res)
+        stack.append(res)
