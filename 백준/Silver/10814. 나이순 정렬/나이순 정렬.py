727f0d69ee7d7cd1d37ab8ef5038e3bf975ab35e
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-11 00:06:02 +0900

    [Silver V] Title: 나이순 정렬, Time: 220 ms, Memory: 45484 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -4,7 +4,7 @@ li=[]
 for i in range(n):
     a, b = sys.stdin.readline().split()
     a=int(a)
-    li.append([a,b])
+    li.append((a,b))
 li.sort(key=lambda x:x[0])
 for i in li:
     print(i[0], i[1])
