8c58a9de5410cff79a55ff76fe436c4fa6decf00
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-11 16:16:43 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,10 +1,11 @@
-n, m=map(int, input().split())
+import sys
+n, m=map(int, sys.stdin.readline().split())
 matrixA=[]
 matrixB=[]
 for _ in range(n):
-    matrixA.append(list(map(int, input().split())))
+    matrixA.append(list(map(int, sys.stdin.readline().split())))
 for _ in range(n):
-    matrixB.append(list(map(int, input().split())))
+    matrixB.append(list(map(int, sys.stdin.readline().split())))
 for r in range(n):
     for c in range(m):
         print(matrixA[r][c]+matrixB[r][c], end=' ')
