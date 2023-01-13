0d0c06f74a9d600e99aacca40e2409b2e7f69180
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-13 14:52:37 +0900

    [Silver V] Title: 단어 정렬, Time: 84 ms, Memory: 35272 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,7 +1,8 @@
-n=int(input())
+import sys 
+n=int(sys.stdin.readline())
 li=[]
 for i in range(n):
-    li.append(input())
+    li.append(sys.stdin.readline().strip('\n'))
 li=list(set(li))
 li.sort(key=lambda x : (len(x), x))
 for i in li:
