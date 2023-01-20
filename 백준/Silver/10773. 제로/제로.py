45c3bb0feeeaeea445f6af85256b95cadf3c4b31
ezcolin2 <105545215+ezcolin2@users.noreply.github.com>
2023-01-20 23:41:55 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 31248 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,10 @@
+import sys
+n=int(sys.stdin.readline())
+stack=[]
+for _ in range(n):
+    temp=int(sys.stdin.readline())
+    if temp==0:
+        stack.pop()
+    else:
+        stack.append(temp)
+print(sum(stack))
