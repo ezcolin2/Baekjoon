6f083fccfdb06b1a0696d95abe42fb1fdafdc808
ez <105545215+ezcolin2@users.noreply.github.com>
2025-04-16 17:06:12 +0900

    [Gold IV] Title: DSLR, Time: 4932 ms, Memory: 214592 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -11,19 +11,9 @@ def command_L(number):
 def command_R(number):
     return number//10 + (number%10)*1000
 
-# command를 연결하여 최종 연산 문자열 구하기
-def get_full_command(first, goal, command_arr, uf):
-    result = []
-    while goal != first:
-        result.append(command_arr[goal])
-        goal = uf[goal]
-    return ''.join(result)[::-1]
 def get_min_command(first, goal):
     # bfs로 해결
-    # command_arr[number] : number가 되기 위해 바로 이전에 적용한 연산
-    command_arr = ["" for _ in range(10000)]
-    # uf[number] : number가 되기 위해 바로 이전 number
-    uf = [i for i in range(10000)]
+    commands_arr = ["" for _ in range(10000)]
     visited = [False for _ in range(10000)]
     queue = deque()
     
@@ -35,33 +25,29 @@ def get_min_command(first, goal):
     while queue:
         number = queue.popleft()
         if number == goal:
-            break
+            return commands_arr[goal]
         # 4가지 연산 수행
         new_number = command_D(number)
         if not visited[new_number]:
             visited[new_number] = True
-            command_arr[new_number] = "D"
-            uf[new_number] = number
+            commands_arr[new_number] = commands_arr[number]+"D"
             queue.append(new_number)
         new_number = command_S(number)
         if not visited[new_number]:
             visited[new_number] = True
-            command_arr[new_number] = "S"
-            uf[new_number] = number
+            commands_arr[new_number] = commands_arr[number]+"S"
             queue.append(new_number)
         new_number = command_L(number)
         if not visited[new_number]:
             visited[new_number] = True
-            command_arr[new_number] = "L"
-            uf[new_number] = number
+            commands_arr[new_number] = commands_arr[number]+"L"
             queue.append(new_number)
         new_number = command_R(number)
         if not visited[new_number]:
             visited[new_number] = True
-            command_arr[new_number] = "R"
-            uf[new_number] = number
+            commands_arr[new_number] = commands_arr[number]+"R"
             queue.append(new_number)
-    return get_full_command(first, goal, command_arr, uf)
+    return commands_arr[goal]
 
 t = int(input())
 for _ in range(t):
